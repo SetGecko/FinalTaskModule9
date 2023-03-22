c45eebc5d5b33379fff178bd5e8c5be9dda7e07c
Алексей <setgecko@gmail.com>
2023-03-22 12:49:08 +0300

    Выполнил итоговые задания

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            var exceptions = new Exception[] { new MyException(), new ArgumentOutOfRangeException(), new StackOverflowException(), new ArgumentNullException(), new AccessViolationException() };
+            foreach (var item in exceptions)
+            {
+                try
+                {
+                    throw new Exception(item.GetType().ToString());
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
         }
+    }
+    public class MyException : Exception
+    {
+
     }
 }
